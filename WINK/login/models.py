62503681b7902fe8d18bd3a287ae7ffbad7ce081
yeonsookim <gjb2628@gmail.com>
2019-09-29 22:43:31 +0900

    completed schedule TestFile

--- WINK/login/models.py
+++ WINK/login/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class LoginTest(models.Model):
+    student_code=models.CharField(max_length=8,blank=True)
+    time = models.CharField(max_length=50,blank=True)
+    A = models.CharField(max_length=50,blank=True)
+    B = models.CharField(max_length=50, blank=True)
+    C = models.CharField(max_length=50, blank=True)
+    D = models.CharField(max_length=50, blank=True)
+    E = models.CharField(max_length=50, blank=True)
+    F = models.CharField(max_length=50, blank=True)
+
+    def __str__(self):
+        return '{} : {}'.format(self.student_code, self.time)
