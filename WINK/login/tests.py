c4abdef4d6de366efdbcdc0dc5c590627b874203
yeonsookim <gjb2628@gmail.com>
2019-09-30 00:08:00 +0900

    add schedule save option db - first login

--- WINK/login/tests.py
+++ WINK/login/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
 # Create your tests here.
+from login.models import UserScheduleDB
+
+model_instance = UserScheduleDB.objects.all()
+model_instance = model_instance.filter(student_code='20153159')
+
+queryset=len(str(model_instance))
+if queryset=='<QuerySet []>':
+    print('True')
+else:
+    print('False')
+print(queryset)
--- WINK/login/views.py
+++ WINK/login/views.py
@@ -17,133 +17,145 @@ options.add_argument('headless')
 driver = webdriver.Chrome(path, chrome_options=options)
 ###############chrome option end
 def Login(request):
-    driver.get('https://ktis.kookmin.ac.kr/')
-    driver.implicitly_wait(3)
-    id = request.POST['id']
-    pw = request.POST['pw']
-    driver.execute_script("document.getElementsByName('txt_user_id')[0].value=\'" + id + "\'")
-    # time.sleep(1)
-    driver.execute_script("document.getElementsByName('txt_passwd')[0].value=\'" + pw + "\'")
-    # time.sleep(1)
-
-    driver.find_element_by_xpath(
-        '/html/body/form[1]/table/tbody/tr/td/table/tbody/tr[4]/td[1]/table/tbody/tr[2]/td/table/tbody/tr[4]/td[2]/input').click()
-    driver.implicitly_wait(3)
-    try:
-        driver.get('https://ktis.kookmin.ac.kr/kmu/ucb.Ucb0164rAGet01.do')
-        html = driver.page_source
-
-        soup = BeautifulSoup(html, 'html.parser')
-        soup.prettify(formatter=lambda s: s.replace('&nbsp', ''))
-        notices = soup.findAll('td', attrs={'rowspan': '2'})
-        notices2 = soup.findAll('td', attrs={'rowspan': '3'})
-        # for n in notices:
-        #     print(n.text)
-        result = ""
-        for n in notices2:
-            print(n.text)
-            result += n.text
-
-        blank = result[0]
-        result = result.replace(blank, '#')
-        result = result[1:]
-        print(result)
-        result += '!'
-        word = ''
-
-        count = 0
-        dataA = ''
-        dataB = ''
-        dataC = ''
-        dataD = ''
-        dataE = ''
-        dataF = ''
-        dataG = ''
-        dataH = ''
-        for i in result:
-            if i != '#':
-                word += i
-            else:
-                if word == '':
-                    word = 'blank'
-                    if count == 0:
-                        dataA = word
-                        count += 1
-                    elif count == 1:
-                        dataB = word
-                        count += 1
-                    elif count == 2:
-                        dataC = word
-                        count += 1
-                    elif count == 3:
-                        dataD = word
-                        count += 1
-                    elif count == 4:
-                        dataE = word
-                        count += 1
-                    elif count == 5:
-                        dataF = word
-                        count += 1
-                    elif count == 6:
-                        dataG = word
-                        count = 0
-                        # print(dataA+dataB+dataC+dataD+dataE+dataF)
-                        print(dataA)
-                        print(dataB)
-                        print(dataC)
-                        print(dataD)
-                        print(dataE)
-                        print(dataF)
-                        print(dataG)
-
-                        dbInstance = UserScheduleDB(student_code='20153159', time=dataA, A=dataB, B=dataC, C=dataD,
-                                                    D=dataE,
-                                                    E=dataF, F=dataG)
-                        dbInstance.save()
-
-                    # print(word)
+    #####check having data
+    model_instance = UserScheduleDB.objects.all()
+    model_instance = model_instance.filter(student_code='20153159')
+    queryset = len(str(model_instance))
 
 
+    if(queryset==13):
+        driver.get('https://ktis.kookmin.ac.kr/')
+        driver.implicitly_wait(3)
+        id = request.POST['id']
+        pw = request.POST['pw']
+        driver.execute_script("document.getElementsByName('txt_user_id')[0].value=\'" + id + "\'")
+        # time.sleep(1)
+        driver.execute_script("document.getElementsByName('txt_passwd')[0].value=\'" + pw + "\'")
+        # time.sleep(1)
+
+        driver.find_element_by_xpath(
+            '/html/body/form[1]/table/tbody/tr/td/table/tbody/tr[4]/td[1]/table/tbody/tr[2]/td/table/tbody/tr[4]/td[2]/input').click()
+        driver.implicitly_wait(3)
+        try:
+            driver.get('https://ktis.kookmin.ac.kr/kmu/ucb.Ucb0164rAGet01.do')
+            html = driver.page_source
+
+            soup = BeautifulSoup(html, 'html.parser')
+            soup.prettify(formatter=lambda s: s.replace('&nbsp', ''))
+            notices = soup.findAll('td', attrs={'rowspan': '2'})
+            notices2 = soup.findAll('td', attrs={'rowspan': '3'})
+            # for n in notices:
+            #     print(n.text)
+            result = ""
+            for n in notices2:
+                print(n.text)
+                result += n.text
+
+            blank = result[0]
+            result = result.replace(blank, '#')
+            result = result[1:]
+            print(result)
+            result += '!'
+            word = ''
+
+            count = 0
+            dataA = ''
+            dataB = ''
+            dataC = ''
+            dataD = ''
+            dataE = ''
+            dataF = ''
+            dataG = ''
+            dataH = ''
+            for i in result:
+                if i != '#':
+                    word += i
                 else:
+                    if word == '':
+                        word = 'blank'
+                        if count == 0:
+                            dataA = word
+                            count += 1
+                        elif count == 1:
+                            dataB = word
+                            count += 1
+                        elif count == 2:
+                            dataC = word
+                            count += 1
+                        elif count == 3:
+                            dataD = word
+                            count += 1
+                        elif count == 4:
+                            dataE = word
+                            count += 1
+                        elif count == 5:
+                            dataF = word
+                            count += 1
+                        elif count == 6:
+                            dataG = word
+                            count = 0
+                            # print(dataA+dataB+dataC+dataD+dataE+dataF)
+                            print(dataA)
+                            print(dataB)
+                            print(dataC)
+                            print(dataD)
+                            print(dataE)
+                            print(dataF)
+                            print(dataG)
 
-                    if count == 0:
-                        dataA = word
-                        count += 1
-                    elif count == 1:
-                        dataB = word
-                        count += 1
-                    elif count == 2:
-                        dataC = word
-                        count += 1
-                    elif count == 3:
-                        dataD = word
-                        count += 1
-                    elif count == 4:
-                        dataE = word
-                        count += 1
-                    elif count == 5:
-                        dataF = word
-                    elif count == 6:
-                        dataG = word
-                        count = 0
+                            dbInstance = UserScheduleDB(student_code='20153159', time=dataA, A=dataB, B=dataC, C=dataD,
+                                                        D=dataE,
+                                                        E=dataF, F=dataG)
+                            dbInstance.save()
 
-                        print(dataA)
-                        print(dataB)
-                        print(dataC)
-                        print(dataD)
-                        print(dataE)
-                        print(dataF)
-                        print(dataG)
-                        # print(dataA + dataB + dataC + dataD + dataE + dataF)
-                        dbInstance = UserScheduleDB(student_code='20153159', time=dataA, A=dataB, B=dataC, C=dataD,
-                                                    D=dataE,
-                                                    E=dataF, F=dataG)
-                        dbInstance.save()
-                    # print(word)
-                    word = ''
+                        # print(word)
+
+
+                    else:
+
+                        if count == 0:
+                            dataA = word
+                            count += 1
+                        elif count == 1:
+                            dataB = word
+                            count += 1
+                        elif count == 2:
+                            dataC = word
+                            count += 1
+                        elif count == 3:
+                            dataD = word
+                            count += 1
+                        elif count == 4:
+                            dataE = word
+                            count += 1
+                        elif count == 5:
+                            dataF = word
+                        elif count == 6:
+                            dataG = word
+                            count = 0
+
+                            print(dataA)
+                            print(dataB)
+                            print(dataC)
+                            print(dataD)
+                            print(dataE)
+                            print(dataF)
+                            print(dataG)
+                            # print(dataA + dataB + dataC + dataD + dataE + dataF)
+                            dbInstance = UserScheduleDB(student_code='20153159', time=dataA, A=dataB, B=dataC, C=dataD,
+                                                        D=dataE,
+                                                        E=dataF, F=dataG)
+                            dbInstance.save()
+                        # print(word)
+                        word = ''
+            return HttpResponseRedirect('/admin/')
+        except:
+            return HttpResponseRedirect('/login/')
+
+
+    ##not first login
+    else:
         return HttpResponseRedirect('/admin/')
-    except:
-        return HttpResponseRedirect('/login/')
 
 
 
