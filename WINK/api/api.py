af9d40a1ed65339d10de01c897ece65b346e9efb
yeonsookim <gjb2628@gmail.com>
2019-10-28 17:59:11 +0900

    devide login & api

--- WINK/api/api.py
+++ WINK/api/api.py
@@ -1,18 +1,16 @@
 import random
 import string
 
+import requests
 from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import AllowAny
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from login import views
 from login.models import UserScheduleDB
 from login.serializers import UserScheduleSerializers
 from login.views import Login
-import requests
+
 
 @api_view(['GET', 'POST'])
 def test(request):
@@ -31,39 +29,6 @@ def test(request):
         print(request.data)
         return Response(status=status.HTTP_200_OK)
 
-@api_view(['POST'])
-@csrf_exempt
-@permission_classes((AllowAny,))
-def logincheck(request):
-    print(request.data)
-
-    if request.method == 'POST':
-        try:
-            id=request.data.get('id')
-            pw=request.data.get('pw')
-
-            views.logincheck(id, pw)
-
-            _LENGTH = 500  # N자리
-
-            # 숫자 + 대소문자
-            string_pool = string.ascii_letters + string.digits
-
-            # 랜덤한 문자열 생성
-            token = ""
-            for i in range(_LENGTH):
-                token += random.choice(string_pool)  # 랜덤한 문자열 하나 선택
-            #print(token)
-            save_session(request, id, pw, token)
-            #print(request.session.get(token, False))
-
-
-
-            print (token)
-            return JsonResponse({'TOKEN' : token})
-        except:
-            print("error")
-            return JsonResponse({'LOGIN' : 'FAIL'})
 
 def save_session(request, id, pw, token):
     print(id, pw)
@@ -85,8 +50,8 @@ def get_user_info(request):
 @api_view(['GET'])
 def createschedule(request):
     if request.method == 'GET':
-        data =request.headers.get('Authorization')
-        data = data[7:]
+        data =request.headers.get('Token')
+
         print(data)
         try:
             userinfo = request.session.get(data, False)
@@ -97,7 +62,7 @@ def createschedule(request):
             serializer = UserScheduleSerializers(data.filter(student_code=userinfo[0]), many=True)
             return JsonResponse({"data":serializer.data},status=status.HTTP_200_OK)
         except:
-            return Response(False,status=status.HTTP_400_BAD_REQUEST)
+            return Response('Fail',status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
 def ktislogin(request):
--- WINK/login/urls.py
+++ WINK/login/urls.py
@@ -1,14 +1,14 @@
 
 
 from django.urls import path
-from login import views, api
+from login import views
+from api import api
 
 urlpatterns = [
     path('', views.index),
     path('logincheck/',views.Login),
     path('afterlogin/',views.afterLogin),
     path('api/createschedule/',api.createschedule),
-    path('api/logincheck/', api.logincheck),
     path('api/getuserinfo/', api.get_user_info),
     path('api/test/', api.test),
     path('api/login/', api.ktislogin),
