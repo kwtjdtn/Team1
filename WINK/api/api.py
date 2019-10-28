e6cafb2e2689bf7545e2817f7aff854eb725a593
yeonsookim <gjb2628@gmail.com>
2019-10-29 01:28:34 +0900

    catched bug

--- WINK/api/api.py
+++ WINK/api/api.py
@@ -1,8 +1,8 @@
-import random
-import string
+import time
 
 import requests
 from django.http import JsonResponse
+from django.utils.crypto import random
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -68,32 +68,27 @@ def createschedule(request):
 def ktislogin(request):
     if request.method == 'POST':
         try:
-            print(request.headers)
-            session = requests.Session()
             with requests.Session() as s:
                 id = request.data.get('id')
                 pw = request.data.get('pw')
                 URL = 'https://ktis.kookmin.ac.kr/kmu/com.Login.do?'
                 data = {'txt_user_id':id, 'txt_passwd':pw}
                 response = s.get(URL,data = data)
-                #print(response.headers)
 
-                #print(response.headers)
                 URL2 = 'https://ktis.kookmin.ac.kr/kmu/ucb.Ucb0164rAGet01.do'
                 custom_headers = {'Set-Cookie':response.headers['Set-Cookie']}
                 response2 = s.post(URL2,custom_headers)
-                #print(response2.text)
+                #print(response2.text) #시간표 긁어온거
 
                 _LENGTH = 500  # N자리
-
                 # 숫자 + 대소문자
-                string_pool = string.ascii_letters + string.digits
-
+                string_pool = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
                 # 랜덤한 문자열 생성
-                token = ""
+                token = str(time.time())
                 for i in range(_LENGTH):
                     token += random.choice(string_pool)  # 랜덤한 문자열 하나 선택
 
+                print(token)
                 save_session(request, id, pw, token)
                 print(request.session.get(token,False))
                 print(token)
