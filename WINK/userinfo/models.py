7abf07e0bdbb2e94e17b2bfbb13ba11d6a9ec7fe
yeonsookim <gjb2628@gmail.com>
2019-11-06 18:43:49 +0900

    modified models

--- WINK/userinfo/models.py
+++ WINK/userinfo/models.py
@@ -56,3 +56,13 @@ class User(AbstractBaseUser, PermissionsMixin):
     def __str__(self):
         #return str(self.id)
         return str(self.name)
+
+class NormalUser(models.Model):
+    id = models.CharField(max_length=20, unique=True, primary_key=True)
+    pw = models.CharField(max_length=20)
+    token = models.CharField(max_length=550, unique=True, null=True)
+    expire = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        #return str(self.id)
+        return str(self.id)
