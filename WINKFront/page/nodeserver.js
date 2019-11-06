3c76d671a009f744bb2f7e7f0db996968482b880
kwtjdtn <43291256+kwtjdtn@users.noreply.github.com>
2019-11-06 23:56:59 +0900

    req error

--- WINKFront/page/nodeserver.js
+++ WINKFront/page/nodeserver.js
@@ -3,17 +3,53 @@ var http = require('http');
 var express = require('express');
 var app = express();
 var fs = require('fs');
-var session = require('express-session');
+const bodyParser = require('body-parser')
+const session = require('express-session');
+const axios = require('axios');
+const FileStore = require('session-file-store')(session);
+
+
+
+//if (request.headers.cookie) {
+//    var cookie = request.headers.cookie.split(';').map(function (element) {
+//        var element = element.split('=');
+//        return {
+//            key: element[0],
+//            value: element[1]
+//        };
+//    });
+//    document.cookie = element;
+//}
+
+const apiHost = "http://localhost:8000/";
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(session({
+    secret: '2$!h2kf66k+f61-#r(ts_v_0ex(h#7+1e@afhxl^ee1#sw@tt2',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 3600000,
+        secure: false,
+        httpOnly: false,
+    },
+}))
 
 app.listen(3000, function (){
     console.log('Server Start');
 });
 
+//router.route('/').get(function (req, res) {
+//    console.log('/');
+//    req.cookies.[Set - Cookie]
+//    res.send()
 app.use(express.static('assets'));
-app.use(express.session({
-    secret: 'mySecret',
-    cookie: { httpOnly: false }
-}));
+
+
+
 
 app.get('/', function (req, res) {
     fs.readFile('wink.html', function (error, data) {
@@ -26,21 +62,46 @@ app.get('/', function (req, res) {
         }
     });
 });
-app.get('/afterlogin', function (req, res) {
+// login/api/ktislogin/
+app.post('/login', function (req, res) {
+    axios.post(apiHost + 'login/api/login/', req.body)
+        .then(response => {
+            let auth_token = response.data.TOKEN;
+            var tkn = auth_token;
+
+            //console.log(req.session)
+            req.session.auth = auth_token;
+            //return response
+            return req.session.save(function () {
+                res.end()
+            })
+        })
+})
+app.get('/afterlogin', function (req, res) { 
     fs.readFile('wink_afterlogin.html', function (error, data) {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end(data);
     });
 });
-
+app.post('/createschedule', function (req, res) {
+    console.log(req.session.auth, req.body);
+    axios.post(apiHost + 'login/api/multiply/',req.body)
+        .then(res => {
+            console.log(res.data);
+            res.end()
+        })
+        .catch(error => {
+            res.end()
+        });
+})
 app.get('/main', function (req, res) {
     fs.readFile('wink_main.html', function (error, data) {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end(data);
     });
-});
+}); 
 
-app.get('/timetalbe', function (req, res) {
+app.get('/timetable', function (req, res) {
     fs.readFile('wink_timetable.html', function (error, data) {
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end(data);
